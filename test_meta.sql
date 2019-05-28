5bcd9f5e5e3cf7b46b541d6acda54da450469dc4
Alex Yang <aleozlx@gmail.com>
2019-05-27 19:47:48 -0500

    redesign table

--- test_meta.sql
+++ test_meta.sql
@@ -1,17 +1,26 @@
-CREATE TABLE partitioning (
+CREATE TABLE partitions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(32) UNIQUE NOT NULL,
-    partitions VARCHAR[] NOT NULL,
+    labels VARCHAR[] NOT NULL,
     created_on TIMESTAMP NOT NULL,
     summary BOOLEAN NOT NULL
 );
 
+-- Example
+-- insert into partitions (name, labels, created_on, summary) values ('traintest', array['train', 'test'], now(), true);
+
 CREATE TABLE frame (
     id SERIAL PRIMARY KEY,
     image TEXT NOT NULL,
-    label INT,
-    active_idx INT,
-    partitions VARCHAR[] NOT NULL
+    class_label INT,
+    ptr INT,
+    partitions hstore
 );
 
 CREATE INDEX idx_frame on frame USING GIN (partitions);
+
+-- Example
+-- insert into frame (image, partitions) values ('a.png', '"train"=>1');
+-- insert into frame (image, partitions) values ('b.png', '"train"=>1');
+-- insert into frame (image, partitions) values ('c.png', '"test"=>1');
+-- select image as train from frame where partitions ? 'train';
