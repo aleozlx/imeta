81581a0aa7f9f8869a23d03b61ed92dc582ffea3
Alex Yang <aleozlx@gmail.com>
2019-05-31 22:37:15 -0500

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE partitions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(32) UNIQUE NOT NULL,
+    labels VARCHAR[] NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    reference TEXT,
+    summary BOOLEAN NOT NULL
+);
+
+CREATE TABLE frame (
+    id SERIAL PRIMARY KEY,
+    image TEXT NOT NULL,
+    class_label INT,
+    ptr INT,
+    partitions hstore
+);
+
+CREATE INDEX idx_frame on frame USING GIN (partitions);
