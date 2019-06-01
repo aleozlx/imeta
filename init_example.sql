917d47ab1b40091de2a539cad3a03911ec952756
Alex Yang <aleozlx@gmail.com>
2019-05-31 22:46:46 -0500

    Update init_example.sql

--- init_example.sql
+++ init_example.sql
@@ -1,5 +1,5 @@
-INSERT INTO frame (image, ptr, partitions) values %s; -- import all data here
+INSERT INTO frame (image, ptr, partitions) VALUES %s; -- import all data here
 
-INSERT INTO partitions (name, labels, created_on, summary) values
+INSERT INTO partitions (name, labels, created_on, summary) VALUES
       ('active', array['active'], now(), true),
       ('part', array['part1', 'part2', 'part3'], now(), true);
