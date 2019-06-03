5f24f4a49befdc5791828e4d369132627c8aaa3a
Alex Yang <aleozlx@gmail.com>
2019-06-03 18:03:13 -0500

    Update summary.sql

--- summary.sql
+++ summary.sql
@@ -8,4 +8,14 @@ SELECT count(*) FROM frame;
 SELECT count(*) FROM frame WHERE partitions ? 'part1';
 
 -- Count total active records in "part1"
-SELECT count(*) FROM frame WHERE partitions ? 'part1' AND partitions ? 'active';
+SELECT count(*) FROM frame WHERE partitions ?& array['part1', 'active'];
+
+-- Count total records in "namespace1"->"part1"
+SELECT count(*) FROM frame where partitions -> 'namespace1' = 'part1';
+
+-- Count total active records in "namespace1"->"part1"
+SELECT count(*) FROM frame WHERE partitions -> 'namespace1' = 'part1' AND partitions ? 'active';
+
+-- Count total active records in "namespace1"
+SELECT count(*) FROM frame WHERE partitions ?& array['namespace1', 'active'];
+
